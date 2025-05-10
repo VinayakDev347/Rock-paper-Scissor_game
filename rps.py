a0323a414848779465e3a4a0ee5f26ccab0476ca
vinayak-dev <vinayakdev347@gmail.com>
2025-05-10 17:30:56 +0530

    added some changes

--- rps.py
+++ rps.py
@@ -2,66 +2,83 @@ import sys
 import random
 from enum import Enum
 
-game_count = 0
+def rock_papper_scissor():
+    game_count = 0
+    player_wins = 0
+    computer_wins = 0
 
 
-def play_game():
+    def play_game():
+        nonlocal player_wins
+        nonlocal computer_wins
 
-    class RPS(Enum):
-        ROCK = 1
-        PAPER = 2
-        SCISSOR = 3
+        class RPS(Enum):
+            ROCK = 1
+            PAPER = 2
+            SCISSOR = 3
 
-    playerchoice=input("Enter your choice...\n----------------------------\n1 for  Rock\n2 for Paper\n3 for Scissor\n---------------------------- ")
+        playerchoice=input("Enter your choice...\n----------------------------\n1 for  Rock\n2 for Paper\n3 for Scissor\n---------------------------- ")
 
-    if playerchoice not in ["1","2","3"]:
-        print("\nYou must enter only 1, 2, 3. Read the Instruction clearly!!!\n")
-        return play_game()
-    
-    player = int(playerchoice)
+        if playerchoice not in ["1","2","3"]:
+            print("\nYou must enter only 1, 2, 3. Read the Instruction clearly!!!\n")
+            return play_game()
+        
+        player = int(playerchoice)
 
-    computerchoice = random.choice("123")
+        computerchoice = random.choice("123")
 
-    computer = int(computerchoice)
+        computer = int(computerchoice)
 
 
-    print("\nYou choose "+ str(RPS(player)).replace('RPS.',' ').title() + ".")
-    print("Computer choose "+ str(RPS(computer)).replace('RPS',' ').title()+".\n")
+        print("\nYou choose "+ str(RPS(player)).replace('RPS.',' ').title() + ".")
+        print("Computer choose "+ str(RPS(computer)).replace('RPS.',' ').title()+".\n")
 
-    def decide_winner(player,computer):
-        if player == 1  and computer == 3:
-            return "You Win"
-        elif player == 2 and computer == 1:
-            return "You Win"
-        elif player == 3 and computer == 2:
-            return "You Win"
-        elif player == computer:
-            return "Its Tiee Game!!!"
+        def decide_winner(player,computer):
+            nonlocal player_wins
+            nonlocal computer_wins
+
+            if player == 1  and computer == 3:
+                player_wins += 1
+                return "You Win"
+            elif player == 2 and computer == 1:
+                player_wins += 1
+                return "You Win"
+            elif player == 3 and computer == 2:
+                player_wins += 1
+                return "You Win"
+            elif player == computer:
+                return "Its Tiee Game!!!"
+            else:
+                computer_wins += 1
+                return "Computer win"
+        
+        game_result = decide_winner(player,computer)
+
+        print(game_result)
+
+        nonlocal game_count
+        game_count += 1
+
+        print("\nGame count: " + str(game_count))
+        print("\nPlayer Wins: " + str(player_wins))
+        print("\nComputer Wins: " + str(computer_wins))
+
+        print("\nPlay again?")
+        while True:
+            playagain = input("\nY for Yes or\nQ for Quit \n\n")
+            if playagain.lower() not in ["y","q"]:
+                continue
+            else:
+                break
+
+        if playagain.lower() == "y":
+            return play_game()
         else:
-            return "Computer win"
-    
-    game_result = decide_winner(player,computer)
+            print("ThankYou for playing!!!")
+            sys.exit("Bye...")
 
-    print(game_result)
+    return play_game
 
-    global game_count
-    game_count += 1
-    print("\nGame count: " + str(game_count))
+play = rock_papper_scissor()
 
-    print("\nPlay again?")
-    while True:
-        playagain = input("\nY for Yes or\nQ for Quit \n\n")
-        if playagain.lower() not in ["y","q"]:
-            continue
-        else:
-            break
-
-    if playagain.lower() == "y":
-        return play_game()
-    else:
-        print("ThankYou for playing!!!")
-        sys.exit("Bye...")
-
-
-
-play_game()
+play()
